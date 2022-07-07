c1bab1e191f7fb65bd057ff4fc50d33edddb7d81
Raghu Krishnan R <rkrsmail@gmail.com>
2022-07-07 13:12:23 +0530

    Initial Commit

---
+++ app/src/main/java/com/rkr/androidassignment/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.rkr.androidassignment.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
---
+++ app/src/main/java/com/rkr/androidassignment/ui/login/LoginViewModelFactory.kt
@@ -0,0 +1,25 @@
+package com.rkr.androidassignment.ui.login
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.rkr.androidassignment.data.LoginDataSource
+import com.rkr.androidassignment.data.LoginRepository
+
+/**
+ * ViewModel provider factory to instantiate LoginViewModel.
+ * Required given LoginViewModel has a non-empty constructor
+ */
+class LoginViewModelFactory : ViewModelProvider.Factory {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(LoginViewModel::class.java)) {
+            return LoginViewModel(
+                loginRepository = LoginRepository(
+                    dataSource = LoginDataSource()
+                )
+            ) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
